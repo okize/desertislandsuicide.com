7cebf2f6fb296b31692af1300050752c8bcda091
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-11-16 22:45:44 -0500

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
 // home page
-exports.index = function(req, res){
+router.get('/', function(req, res) {
   res.render('index', { title: 'Desert Island Suicide' });
-};
+});
 
 // subscription confirmation
-exports.subscribed = function(req, res){
+router.get('/subscribed', function(req, res) {
   res.render('index', { title: 'Subscription confirmed!', subscribed: true });
-};
+});
+
+module.exports = router;
