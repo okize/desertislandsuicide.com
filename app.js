9796203da2ba384d61828614e9f40916cfe09fdc
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-11-17 08:04:09 -0500

    update main app file for express 4

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
-if (process.env.NODE_ENV === 'production') {
-  require('newrelic');
-}
-
-// modules
-var express = require('express');
-var routes = require('./routes');
 var path = require('path');
-
+var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var routes = require('./routes');
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4444);
+
+// view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+// logger
+app.use(logger('dev'));
+
+// static assets
+app.use(favicon(__dirname + '/public/favicon/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+// routes
+app.use('/', routes);
+app.use('/subscribed', routes);
+
+// catch 404s
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-app.get('/', routes.index);
-app.get('/subscribed', routes.subscribed);
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: {}
+      });
+  });
+}
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
